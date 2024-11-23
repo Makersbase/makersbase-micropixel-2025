4cebb611aaae65e6beed09f36acb919a659b6149
Makersbase <148645971+Makersbase@users.noreply.github.com>
2024-11-23 14:53:56 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,9 @@ namespace microPixel {
     /**
      * Maakt de Sprite aan
      * 
+     * 
      */
-    
     export function maakSprite(x: string): void {
-        
+        basic.showString(x)
     }
 }
